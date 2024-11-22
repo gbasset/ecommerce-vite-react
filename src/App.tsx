b1ba6a6d2c4a9afd65c517d29f61ed7bca010d7a
gaetan basset <51761442+gbasset@users.noreply.github.com>
2024-11-22 17:09:22 +0100

    feat(design-system): add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import Button from 'common/design-system/Button/Button.tsx';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <div>
-                <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-                    <img src={viteLogo} className='logo' alt='s' />
-                </a>
-                <a href='https://react.dev' target='_blank' rel='noreferrer'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
+            <Button variant={'contained'} onClick={() => console.log('bla')}>
+                Ajouter au panier
+            </Button>{' '}
+            <Button
+                // variant={'contained'}
+                onClick={() => console.log('bla')}
+            >
+                Retirer du panier
+            </Button>
         </>
     );
 }
