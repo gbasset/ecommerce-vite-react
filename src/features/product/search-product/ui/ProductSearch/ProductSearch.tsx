fd6aaf103af7131c766c7afe222804ad831629f1
gbasset <51761442+gbasset@users.noreply.github.com>
2026-06-04 10:43:44 +0200

    feat(product-search): implement search functionality in ProductSearch component

--- src/features/product/search-product/ui/ProductSearch/ProductSearch.tsx
+++ src/features/product/search-product/ui/ProductSearch/ProductSearch.tsx
@@ -1,14 +1,28 @@
-import type { ReactElement } from 'react';
+import type { ReactElement, ChangeEvent, FormEvent } from 'react';
 import { Search } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
-export const ProductSearch = (): ReactElement => {
-    return <form>
+import { useState } from 'react';
+type Props = {
+    onSubmit: (search: string) => void;
+};
+export const ProductSearch = ({ onSubmit }: Props): ReactElement => {
+    const [search, setSearch] = useState('');
+    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+    };
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        onSubmit(search);
+    };
+    return <form onSubmit={handleSubmit}>
         <input 
             type="text" 
             id="product-search"
             name="product-search"
             placeholder="Rechercher un produit" 
             aria-label="Rechercher un produit"
+            value={search}
+            onChange={handleSearch}
         />
         <IconButton type="submit" aria-label="Rechercher un produit">
             <Search />
