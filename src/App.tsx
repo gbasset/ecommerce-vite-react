c8ec2335ea531f6df98382c9acc61c1b480a8b65
gbasset <51761442+gbasset@users.noreply.github.com>
2026-06-24 22:22:46 +0200

    feat(list-products): create components productItem and ProductList

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
 import Header from 'common/layout/Header/Header';
+import ProductList from 'features/product/list-product/ProductList';
+import { Product } from 'features/product/list-product/types/types';
 function App() {
     const handleSubmit = (search: string) => {
         console.log(search);
     };
     const cartCount = 3;
-    return (
+    const products: Product[] = [{
+        id: '1',
+        name: 'Product 1',
+        price: 100,
+        picture: '',
+    },
+    {
+        id: '2',
+        name: 'Product 2',
+        price: 200,
+        picture: '',
+    },
+    {
+        id: '3',
+        name: 'Product 3',
+        price: 300,
+        picture: '',
+    },
+    {
+        id: '4',
+        name: 'Product 4',
+        price: 400,
+        picture: '',
+    },
+    {
+        id: '5',
+        name: 'Product 5',
+        price: 500,
+        picture: '',
+    },
+];
+    return (<>
+    
             <Header onSubmit={handleSubmit} cartCount={cartCount} />
+                <ProductList products={products} />
+    </>
     );
 }
 
