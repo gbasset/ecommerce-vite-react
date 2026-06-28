45322fbb2cdaf690c9c9fce95e3c287712429737
gbasset <51761442+gbasset@users.noreply.github.com>
2026-06-28 21:02:56 +0200

    feat(CartProductList): add CartProductList

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from 'common/layout/Header/Header';
 
-import { CartProductItem } from 'features/cart/list-cart-products/ui/CartProductItem';
 import { CartProductItemData } from 'features/cart/list-cart-products/types';
+import { CartProductList } from 'features/cart/list-cart-products/ui';
 function App() {
     const handleSubmit = (search: string) => {
         console.log(search);
@@ -10,18 +10,43 @@ function App() {
         console.log('remove from cart');
     };
     const cartCount = 3;
-    const products: CartProductItemData = {
-        name: 'Product 1',
-        price: 100,
+    const products: CartProductItemData[] = [
+        {
+            id: '1',
+            name: 'Product 1',
+            price: 100,
+            picture: '',
+        },
+    {
+        id: '2',
+        name: 'Product 2',
+        price: 200,
         picture: '',
-        id: '1',
-    };
-
+    }, 
+    {
+        id: '3',
+        name: 'Product 3',
+        price: 300,
+        picture: '',
+    },
+    {
+        id: '4',
+        name: 'Iphone 15',
+        price: 1500,
+        picture: '',
+    },
+    {
+        id: '5',
+        name: 'Iphone 16',
+        price: 1600,
+        picture: '',
+    }
+];
     return (<>
     
             <Header onSubmit={handleSubmit} cartCount={cartCount} />
             <div className='m-5'>
-                <CartProductItem cartProduct={products} removeFromCart={handleRemoveFromCart} />
+                <CartProductList cartProducts={products} removeFromCart={handleRemoveFromCart} />
             </div>
     </>
     );
