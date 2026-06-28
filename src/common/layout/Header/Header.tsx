1096c52adfc808f34a191d1246a842f3bf5a9a05
gbasset <51761442+gbasset@users.noreply.github.com>
2026-06-28 23:50:13 +0200

    feat(Routing): add routing by product

--- src/common/layout/Header/Header.tsx
+++ src/common/layout/Header/Header.tsx
@@ -9,15 +9,13 @@ type Props = {
 };
 function Header ({ onSubmit, cartCount }: Props): ReactElement {
     return (
-        <header className="bg-primary flex flex-col p-4 lg:flex-row lg:items-center lg:justify-between lg:gap-4">
+        <header className="bg-primary flex flex-col p-4 lg:flex-row lg:items-center lg:gap-4">
             <div className="flex items-center justify-between w-full mb-8 lg:mb-0 lg:contents">
-                <Link to="/" className="text-white font-bold ml-4 lg:ml-4 lg:order-1 lg:shrink-0">
+                <Link to="/" className="text-white font-bold ml-4 lg:order-1 lg:shrink-0">
                     Tech shop
                 </Link>
-                <Link to="/cart" >
-                <div className="lg:order-3 lg:shrink-0">
+                <Link to="/cart" className="lg:order-3 lg:shrink-0">
                     <CartCount cartCount={cartCount} />
-                </div>
                 </Link>
             </div>
             <div className="flex justify-center w-full lg:order-2 lg:flex-1">
--- src/features/product/list-product/ui/ProductList/ProductList.tsx
+++ src/features/product/list-product/ui/ProductList/ProductList.tsx
@@ -1,6 +1,7 @@
 import {ReactElement} from 'react';
 import { Product } from '../../types/types';
 import { ProductItem } from '../ProductItem/ProductItem';
+import { Link } from 'react-router-dom';
 type Props = {
     products: Product[];
 };
@@ -9,7 +10,9 @@ const ProductList = ({products}: Props): ReactElement => {
         <div className='flex flex-col items-center justify-center sm:flex-row sm:flex-wrap sm:justify-center gap-2' >
         {products.map((product) => {
         const {id, ...productWithoutId} = product;
-        return <ProductItem key={id} product={productWithoutId} />
+        return <Link to={`/product/${id}`} key={id}>
+        <ProductItem product={productWithoutId} />
+        </Link>
     })} </div>
     );
 };
