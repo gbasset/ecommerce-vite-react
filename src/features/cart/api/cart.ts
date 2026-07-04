373a7f0cc47d6e064448794766a3e2fe336a7ab6
gbasset <51761442+gbasset@users.noreply.github.com>
2026-07-04 19:04:43 +0200

    feat(cart): define  api promise

---
+++ src/features/cart/api/cart.ts
@@ -0,0 +1,28 @@
+import type { CartProductItemData } from '../list-cart-products/types';
+import { productToAddToCart } from '../add-cart-product/data/productToAddToCart';
+type ChangeInCartResponse = {
+cartProducts: CartProductItemData[];
+cartCount: number;
+};
+let cartProducts: CartProductItemData[] = [];
+let cartCount = 0;
+export const getCartCount = (): number => {return cartCount;}
+export const getCartProducts = (): CartProductItemData[] => {return cartProducts;}
+export const addProductToCart = (productId: string): Promise<ChangeInCartResponse> => {
+    const isProductInCart = cartProducts.some((product) => product.id
+    === productId);
+    if (!isProductInCart) {
+    const productToAdd = productToAddToCart[productId];
+    cartProducts = [...cartProducts, productToAdd];
+    cartCount += 1;
+    }
+    return Promise.resolve({ cartProducts, cartCount });
+};
+    export const removeProductFromCart = (
+    productId: CartProductItemData['id'],
+    ): Promise<ChangeInCartResponse> => {
+    cartProducts = cartProducts.filter((cartProduct) => cartProduct.id
+    !== productId);
+    cartCount -= 1;
+    return Promise.resolve({ cartProducts, cartCount });
+    };
